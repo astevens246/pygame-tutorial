f4e0d0fa6152fa22a600fc515ab13ca060e02986
Allen Stevens <allen.stevens@students.dominican.edu>
2023-11-13 15:33:15 -0800

    added moving apple and strawberry

--- drawing-objects.py
+++ drawing-objects.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-member
-
+from random import randint
+import random
 # Import and initialize pygame
 import pygame
 # Get the clock
@@ -15,13 +16,48 @@ class GameObject(pygame.sprite.Sprite):
   def render(self, screen):
     screen.blit(self.surf, (self.x, self.y))
 
+class Apple(GameObject):
+    def __init__(self):
+        super(Apple, self).__init__(0, -64, 'apple.png')  # Start from top with a random x position
+        self.dx = 0
+        self.dy = (randint(1, 3))  # Move downward
+        self.reset()
+
+    def move(self):
+        self.y += self.dy
+        if self.y > 500:
+            self.reset()
+
+    def reset(self):
+        self.x = random.choice([93, 218, 343])
+        self.y = -64
+
+class Strawberry(GameObject):
+    def __init__(self):
+        super(Strawberry, self).__init__(0, -64, 'strawberry.png')  # Start from left with a random y position
+        self.dx = randint(1, 3)  # Move from left to right
+        self.dy = 0
+        self.reset()
+
+    def move(self):
+        self.x += self.dx
+        if self.x > 500:
+            self.reset()
+
+    def reset(self):
+        self.x = random.choice([93, 343])  # Randomly choose between 93 and 343 for x
+        self.y = randint(0, 500)
+
+
+    
 pygame.init()
 
   
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
-
-apple1 = GameObject(0, 250, 'apple.png')
+# Create the apple object
+apple = Apple()
+strawberry = Strawberry()
 
 
 # Create the game loop
@@ -33,10 +69,15 @@ while running:
       running = False
       # Clear screen
   screen.fill((0, 0, 0))
+  # Draw apple
+  apple.move()
+  apple.render(screen)
+  #Draw strawberry 
+  strawberry.move()
+  strawberry.render(screen)
 
-  # Draw the box object
-  apple1.x += 2
-  apple1.render(screen)
+
+
 
   # Update the window
   pygame.display.flip()
